d302d51ba017ee066bd76bed9e86d92d9850bef2
kikito <kikito@gmail.com>
2015-04-17 20:16:08 +0200

    Fix several problems with CameraMan initialisation

--- gamestates/play.lua
+++ gamestates/play.lua
@@ -9,9 +9,11 @@ function Play:enteredState()
   self.world = bump.newWorld()
   local width, height = 2000, 2000
 
-  self.camera = CameraMan:new(world, width, height)
+  self.camera = CameraMan:new(self.world, width, height)
 
   self.map = Map:new(self.world, width, height, self.camera)
+
+  self.camera:setPosition(self.map:getPlayerPosition())
 
   love.audio.setDistanceModel("linear clamped")
 end
@@ -30,7 +32,9 @@ end
 
 function Play:update(dt)
   self.map:update(dt, self.camera:getVisible())
-  self.camera:setTarget(self.map:getPlayerPosition())
+  local cx,cy = self.map:getPlayerPosition()
+  self.camera:setTarget(cx,cy)
+  self.camera:reactToShakers(cx,cy)
   self.camera:update(dt)
 end
 
